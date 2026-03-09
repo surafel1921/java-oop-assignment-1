f3e89c3e950ea80894c1529201c1456694a54d9e
Tesfamichael-G <tesfamichael.gegziabher@aait.edu.et>
2026-03-09 23:06:03 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,1 +1,22 @@
 
+public class Main {
+    public static void main(String[] args) {
+        // --- TODO: Step 1 ---
+        // Create your Student.java file in the src/ folder.
+        // It should have private fields, a constructor, and getters/setters.
+
+        // --- TODO: Step 2 ---
+        // Instantiate your first student object here
+        // Example: Student s1 = new Student("Abebe Kassa", 12345);
+
+        // --- TODO: Step 3 ---
+        // Instantiate your second student object here
+        
+        // --- TODO: Step 4 ---
+        // Print the details to the console using your getter methods
+        System.out.println("--- Student Enrollment List ---");
+        // System.out.println("Student Name: " + s1.getName());
+
+        System.out.println("\nAssignment Status: Check your Git commits before pushing!");
+    }
+}
